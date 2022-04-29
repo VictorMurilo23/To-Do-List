b26b4f8db68e338e5b20ec2c69ed91cce7b8c0ba
Victor Murilo <victor2706.vm@gmail.com>
2022-04-29 12:27:57 -0300

    Cumpri o requisito 6 e 7

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ botaoCriarTarefa.addEventListener('click', () => {
   document.querySelector('#lista-tarefas').appendChild(li);
   inputTexto.value = '';
 });
+
+document.querySelector('#lista-tarefas').addEventListener('click', (event) => {
+  // eslint-disable-next-line no-param-reassign
+  event.target.style.backgroundColor = 'gray';
+});
