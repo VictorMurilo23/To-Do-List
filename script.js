9100aca57f71fdb2cab6d9c139844b8e79d77ef5
Victor Murilo <victor2706.vm@gmail.com>
2022-04-30 13:11:56 -0300

    Cumpri o requisito 12

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+const listaToda = JSON.parse(localStorage.getItem('listaSalva'));
 const botaoCriarTarefa = document.querySelector('#criar-tarefa');
 const inputTexto = document.querySelector('#texto-tarefa');
 const listaTarefa = document.querySelector('#lista-tarefas');
+
+function carregarListaSalva() {
+  for (let index = 0; index < Object.keys(listaToda).length; index += 1) {
+    const lis = document.createElement('li');
+    lis.innerText = listaToda[index]['0'];
+    lis.className = listaToda[index]['1'];
+    listaTarefa.appendChild(lis);
+  }
+}
+
+window.onload = function verificadorListaSalva() {
+  if (listaToda !== null) {
+    carregarListaSalva();
+  }
+};
 
 botaoCriarTarefa.addEventListener('click', () => {
   const li = document.createElement('li');
@@ -41,6 +57,17 @@ document.querySelector('#remover-finalizados').addEventListener('click', () => {
 });
 
 document.querySelector('#remover-selecionado').addEventListener('click', () => {
-  const selecionado = document.querySelector('.itemSelecionado');
-  selecionado.remove();
+  document.querySelector('.itemSelecionado').remove();
+});
+
+// eslint-disable-next-line complexity
+document.querySelector('#salvar-tarefas').addEventListener('click', () => {
+  const tarefas = {};
+  const itensLista = document.querySelectorAll('li');
+  for (let index = 0; index < itensLista.length; index += 1) {
+    if (itensLista[index].innerText !== '') { // vai impedir de salvar itens vazios
+      tarefas[index] = [itensLista[index].innerText, itensLista[index].className];
+    }
+  }
+  localStorage.setItem('listaSalva', JSON.stringify(tarefas)); // https://blog.logrocket.com/localstorage-javascript-complete-guide/ <-- me ajudou muito nessa parte :)
 });
